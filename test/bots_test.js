c4033f551cfc40316b4f59742f8aa94c26273835
juan-pvb <juan@petvetbook.com>
2016-02-12 14:15:53 +0200

    Added test for google adsense

--- test/bots_test.js
+++ test/bots_test.js
@@ -114,4 +114,42 @@ exports['Pingdom Bot'] = function(test) {
 
     test.done();
 
+};
+
+exports['Adsense Bot'] = function(test) {
+
+    var source = '(compatible; Mediapartners-Google/2.1; +http://www.google.com/bot.html)';
+
+    var userAgent = ua.parse(source);
+
+    test.ok(!userAgent.isMobile, 'Mobile');
+    test.ok(!userAgent.isiPad, 'iPad');
+    test.ok(!userAgent.isiPod, 'iPod');
+    test.ok(!userAgent.isiPhone, 'iPhone');
+    test.ok(!userAgent.isAndroid, 'Android');
+    test.ok(!userAgent.isBlackberry, 'Blackberry');
+    test.ok(!userAgent.isOpera, 'Opera');
+    test.ok(!userAgent.isIE, 'IE');
+    test.ok(!userAgent.isSafari, 'Safari');
+    test.ok(!userAgent.isFirefox, 'Firefox');
+    test.ok(!userAgent.isWebkit, 'Webkit');
+    test.ok(!userAgent.isChrome, 'Chrome');
+    test.ok(!userAgent.isKonqueror, 'Konqueror');
+    test.ok(!userAgent.isOmniWeb, 'OmniWeb');
+    test.ok(!userAgent.isSeaMonkey, 'SeaMonkey');
+    test.ok(!userAgent.isFlock, 'Flock');
+    test.ok(!userAgent.isAmaya, 'Amaya');
+    test.ok(!userAgent.isEpiphany, 'Epiphany');
+    test.ok(!userAgent.isDesktop, 'Desktop');
+    test.ok(!userAgent.isWindows, 'Windows');
+    test.ok(!userAgent.isLinux, 'Linux');
+    test.ok(!userAgent.isMac, 'Mac');
+    test.ok(!userAgent.isBada, 'Bada');
+    test.ok(!userAgent.isSamsung, 'Samsung');
+    test.ok(!userAgent.isRaspberry, 'Raspberry');
+    test.ok(userAgent.isBot, 'Bot');
+    test.ok(!userAgent.isAndroidTablet, 'AndroidTablet');
+
+    test.done();
 }
+
